b106836b4aac8e6db5e4bb15164539f799dd75f9
RainVN <rainvn.cb@gmail.com>
2022-04-28 21:21:27 +0700

    Implemented change information button in navigation bar

--- src/components/NavigationHeader/NavigationHeader.jsx
+++ src/components/NavigationHeader/NavigationHeader.jsx
@@ -1,5 +1,5 @@
 import {Header} from "antd/es/layout/layout";
-import {Card, Dropdown, Image, Menu, Modal} from "antd";
+import {Button, Card, Dropdown, Form, Image, Input, Menu, Modal} from "antd";
 import {Link, NavLink} from "react-router-dom";
 import {
     URL_ADMIN_DASHBOARD,
@@ -18,9 +18,10 @@ import SharedContext from "../../utils/Context";
 import {useHistory} from "react-router";
 import {CaretDownFilled} from "@ant-design/icons"
 import {USER_INFO} from "../../const/key_storage";
+import {userGetInfo, userUpdateUserInfo} from "../../api/api_config";
 
 const NavigationHeader = () => {
-    const {token, userInfo, isAdmin} = useContext(SharedContext)
+    const {token, userInfo, isAdmin, isSessionExpired} = useContext(SharedContext)
     const history = useHistory()
     const menu = (
         <Menu>
@@ -45,6 +46,95 @@ const NavigationHeader = () => {
         </Menu>
     );
 
+    const onChangeInformation = async () => {
+        let inputData = (JSON.parse(userInfo.get)).user
+        Modal.info({
+            title: "Edit info",
+            width: 600,
+            icon: <div/>,
+            centered: true,
+            content: (
+                <Form
+                    labelCol={{span: 8}}
+                    wrapperCol={{span: 16}}
+                    initialValues={{remember: true}}
+                    onFinish={async () => {
+                        try {
+                            const res = await userUpdateUserInfo({
+                                id: inputData.id,
+                                firstName: inputData.firstName,
+                                lastName: inputData.lastName,
+                                email: inputData.email,
+                                address: inputData.address,
+                            })
+                            if (!res.ok) Modal.error({title: "Error", content: "Nothing changed!"})
+                            else {
+                                const json = await res.json()
+                                if (json.status === 0) Modal.success({
+                                    title: "Edit successfully",
+                                    onOk: () => Modal.destroyAll()
+                                })
+                            }
+                        } catch (TypeError) {
+                            Modal.error(
+                                {
+                                    title: "Login session expired",
+                                    content: "Please login again",
+                                    onOk: () => {
+                                        isSessionExpired.set(true)
+                                    }
+                                },)
+                        } finally {
+                            const json = await (await userGetInfo()).json()
+                            userInfo.set(JSON.stringify(json))
+                            window.localStorage.setItem(USER_INFO, JSON.stringify(json))
+                        }
+                    }}>
+                    <Form.Item
+                        label="First name:"
+                        name="firstName"
+                        initialValue={inputData.firstName}
+                        rules={[{required: true, message: 'Please input your firstname!'}]}>
+                        <Input onChange={(e) => inputData.firstName = e.currentTarget.value}/>
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Last name:"
+                        name="lastName"
+                        initialValue={inputData.lastName}
+                        rules={[{required: true, message: 'Please input your last name!'}]}>
+                        <Input
+                            onChange={(e) => inputData.lastName = e.currentTarget.value}/>
+                    </Form.Item>
+                    <Form.Item
+                        label="Email:"
+                        name="email"
+                        initialValue={inputData.email}
+                        rules={[{required: true, message: 'Please input your email!'}]}>
+                        <Input
+                            onChange={(e) => inputData.email = e.currentTarget.value}/>
+                    </Form.Item>
+                    <Form.Item
+                        label="Address:"
+                        name="address"
+                        initialValue={inputData.address}
+                        rules={[{required: true, message: 'Please input your address!'}]}>
+                        <Input onChange={(e) => inputData.address = e.currentTarget.value}/>
+                    </Form.Item>
+                    <Form.Item wrapperCol={{offset: 8, span: 16}}>
+                        <div style={{display: "flex", justifyContent: "flex-start", alignItems: "center"}}>
+                            <Button type="primary" htmlType="submit">
+                                Update
+                            </Button>
+                            <Button style={{marginLeft: 50}} htmlType="button" onClick={() => Modal.destroyAll()}>
+                                Cancel
+                            </Button>
+                        </div>
+                    </Form.Item>
+                </Form>),
+            okButtonProps: {style: {display: "none"}}
+        })
+    }
 
     const onClickLogout = () => {
         Modal.confirm({
@@ -64,7 +154,7 @@ const NavigationHeader = () => {
     const userMenu = (
         <Menu>
             <Menu.Item key="0">
-                <Link to={"#"}>Change information</Link>
+                <Link to={"#"} onClick={onChangeInformation}>Change information</Link>
             </Menu.Item>
             <Menu.Item key="1">
                 <Link to={"#"} onClick={onClickLogout}>Logout</Link>
@@ -93,8 +183,9 @@ const NavigationHeader = () => {
                 flexDirection: "row",
                 margin: 0
             }}>
-                <Image style={{display: "flex", alignItems: "center"}} preview={false} height={60} src={"ic_hanu_banking.png"}/>
-                <h2 style={{margin: 0, color: "#6b6b6b"}}><b style={{color :"#58b893"}}>HANU</b> Banking</h2>
+                <Image style={{display: "flex", alignItems: "center"}} preview={false} height={60}
+                       src={"ic_hanu_banking.png"}/>
+                <h2 style={{margin: 0, color: "#6b6b6b"}}><b style={{color: "#58b893"}}>HANU</b> Banking</h2>
             </div>
             <Menu style={{flexGrow: 2, display: "flex", justifyContent: "flex-end", borderBottom: "none"}}
                   mode="horizontal">
